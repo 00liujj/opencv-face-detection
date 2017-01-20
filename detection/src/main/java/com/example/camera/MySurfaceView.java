97769e4943c80c5896a2b486d8e5b1375486d8e3
Jianjun Liu <00liujj@163.com>
2017-01-21 00:29:11 +0800

    可以用surfaceview，也可以用camera了。

--- detection/src/main/java/com/example/camera/MySurfaceView.java
+++ detection/src/main/java/com/example/camera/MySurfaceView.java
@@ -15,11 +15,13 @@ import android.view.SurfaceView;
 import java.io.IOException;
 import java.util.List;
 
+import android.os.SystemClock;
+
 /**
  * Created by cpxiao on 15/9/19.
  */
 public class MySurfaceView extends SurfaceView {
-    private static final String TAG = "MySurfaceView";
+    private static final String TAG = "detection.MySurfaceView";
 
     //SurfaceHolder用于控制SurfaceView的大小、格式等，用于监听SurfaceView的状态。
     private SurfaceHolder mSurfaceHolder;
@@ -46,7 +48,7 @@ public class MySurfaceView extends SurfaceView {
 
     private void init() {
         //实例SurfaceHolder
-        Log.d(MainActivity.TAG, "Init My surface view");
+        Log.d(TAG, "Init My surface view");
         mSurfaceHolder = getHolder();
         //为SurfaceView添加状态监听
         mSurfaceHolder.addCallback(new MyCallback2());
@@ -89,77 +91,117 @@ public class MySurfaceView extends SurfaceView {
 
     class MyCallback2 implements SurfaceHolder.Callback {
 
-        Camera camera;
+        Camera mCamera = null;
+        boolean mPreview = false;
+        byte[] mBuffer = null;
 
         @Override
         public void surfaceCreated(SurfaceHolder surfaceHolder) {
-            camera = Camera.open();
-            if (camera == null) {
-                Log.d(MainActivity.TAG, "the camera is null");
+            Log.d(TAG, "surfaceCreated");
+
+            mCamera = Camera.open();
+            if (mCamera != null) {
+                Log.d(TAG, "Camera opened");
+            } else {
+                Log.d(TAG, "failed to open Camera");
+                return;
             }
+            int surfaceWidth = surfaceHolder.getSurfaceFrame().width();
+            int surfaceHeight = surfaceHolder.getSurfaceFrame().height();
+            Camera.Parameters params = mCamera.getParameters();
+            Log.d(TAG, "getSupportedPreviewSizes()");
+            List<Camera.Size> supportedSizes = params.getSupportedPreviewSizes();
+
+            if (supportedSizes != null) {
+                /* Select the size that fits surface considering maximum size allowed */
+                int calcWidth = 0;
+                int calcHeight = 0;
+
+                for (Camera.Size size : supportedSizes) {
+                    int width = size.width;
+                    int height = size.height;
+
+                    if (width <= surfaceWidth && height <= surfaceHeight) {
+                        if (width >= calcWidth && height >= calcHeight) {
+                            calcWidth = width;
+                            calcHeight = height;
+                        }
+                    }
+                }
+                Log.d(TAG, "Set mPreview size to " + calcWidth + "x" + calcHeight);
+                params.setPreviewSize(calcWidth, calcHeight);
+            }
+
+            List<String> focusModes = params.getSupportedFocusModes();
+            if (focusModes != null && focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO)) {
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+            }
+            params.setPreviewFormat(ImageFormat.NV21);
+            //params.setPreviewFrameRate(5);
+
+            mCamera.setParameters(params);
+
+            mCamera.setDisplayOrientation(90);
+
+
+
+            try {
+                //mCamera.setPreviewDisplay(null);
+                mCamera.setPreviewDisplay(mSurfaceHolder);
+            } catch (IOException e) {
+                mCamera.release();
+                mCamera = null;
+            }
+
+            params = mCamera.getParameters();
+
+            int mFrameWidth = params.getPreviewSize().width;
+            int mFrameHeight = params.getPreviewSize().height;
+
+            int size = mFrameWidth * mFrameHeight;
+            size = size * ImageFormat.getBitsPerPixel(params.getPreviewFormat()) / 8;
+            Log.d(TAG, String.format("need %d bytes", size));
+            mBuffer = new byte[size];
+
+            mCamera.addCallbackBuffer(mBuffer);
+            mCamera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback() {
+            //mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+                @Override
+                public void onPreviewFrame(byte[] bytes, Camera camera) {
+                    Log.d(TAG, "onPreviewFrame received " + bytes.length);
+                    Log.d(TAG, "mCamera is " + mCamera + ", camera is " + camera);
+                    SystemClock.sleep(3000);
+                    // 这个buffer会保留一段时间。直到下一次调用。
+                    if (camera != null) {
+                        camera.addCallbackBuffer(mBuffer);
+                    }
+                }
+            });
+
+
+            /* Finally we are ready to start the mPreview */
+            Log.d(TAG, "startPreview");
+            mCamera.startPreview();
+            mPreview = true;
         }
 
         @Override
-        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-            Camera.Parameters params = camera.getParameters();
-            Log.d(TAG, "getSupportedPreviewSizes()");
-            List<Camera.Size> sizes = params.getSupportedPreviewSizes();
-
-            if (sizes != null) {
-                    /* Select the size that fits surface considering maximum size allowed */
-                Size frameSize = calculateCameraFrameSize(sizes, new JavaCameraSizeAccessor(), width, height);
-
-                params.setPreviewFormat(ImageFormat.NV21);
-                Log.d(TAG, "Set preview size to " + Integer.valueOf((int)frameSize.width) + "x" + Integer.valueOf((int)frameSize.height));
-                params.setPreviewSize((int)frameSize.width, (int)frameSize.height);
-
-
-                List<String> FocusModes = params.getSupportedFocusModes();
-                if (FocusModes != null && FocusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO))
-                {
-                    params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
-                }
-
-                camera.setParameters(params);
-                params = camera.getParameters();
-
-                mFrameWidth = params.getPreviewSize().width;
-                mFrameHeight = params.getPreviewSize().height;
-
-//                if ((getLayoutParams().width == LayoutParams.MATCH_PARENT) && (getLayoutParams().height == LayoutParams.MATCH_PARENT))
-//                    mScale = Math.min(((float)height)/mFrameHeight, ((float)width)/mFrameWidth);
-//                else
-//                    mScale = 0;
-
-                int size = mFrameWidth * mFrameHeight;
-                size  = size * ImageFormat.getBitsPerPixel(params.getPreviewFormat()) / 8;
-                mBuffer = new byte[size];
-
-                mCamera.addCallbackBuffer(mBuffer);
-                mCamera.setPreviewCallbackWithBuffer(this);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    mSurfaceTexture = new SurfaceTexture(MAGIC_TEXTURE_ID);
-                    mCamera.setPreviewTexture(mSurfaceTexture);
-                } else
-                    mCamera.setPreviewDisplay(null);
-
-                    /* Finally we are ready to start the preview */
-                Log.d(TAG, "startPreview");
-                mCamera.startPreview();
-            }
-            try {
-                camera.setPreviewDisplay(mSurfaceHolder);
-            } catch (IOException e) {
-                camera.release();
-                camera = null;
-            }
-            camera.startPreview();
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int surfaceWidth, int surfaceHeight) {
+            Log.d(TAG,"surfaceChanged");
         }
 
         @Override
         public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-            camera.release();
+            Log.d(TAG,"surfaceDestroyed");
+            if (mCamera != null) {
+                if (mPreview) {
+                    mCamera.stopPreview();
+                    mPreview = false;
+                }
+                mCamera.setPreviewCallback(null);
+                mCamera.release();
+                mCamera = null;
+            }
         }
     }
 
@@ -190,9 +232,10 @@ public class MySurfaceView extends SurfaceView {
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //Log.d(TAG,"onTouchEvent");
         textX = (int) event.getX();
         textY = (int) event.getY();
-        myDraw();
+        //myDraw();
         return true;
 //      return super.onTouchEvent(event);
     }
